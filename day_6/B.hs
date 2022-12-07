ff2e7b3cce601b12a6ea32215ad894ed18ff9b6d
Saulius Beinorius <saulius.beinorius@gmail.com>
2022-12-07 15:47:10 +0200

    Add solution for day 7

---
+++ day_6/B.hs
@@ -0,0 +1,33 @@
+#!/usr/bin/env stack
+{- stack script
+ --resolver lts-20.3
+ --install-ghc
+ --package "containers"
+-}
+
+import Data.Function
+import qualified Data.List as List
+import Data.Maybe
+import qualified Data.Set as Set
+import System.Environment
+
+slidingWindow :: Int -> [a] -> [[a]]
+slidingWindow _ [] = []
+slidingWindow size list@(_:others)
+    | length currentSlice == size = currentSlice : (slidingWindow size others)
+    | otherwise = []
+    where currentSlice = take size list
+
+main = do
+    (inputPath:_) <- getArgs
+    contents <- readFile inputPath
+    let windowSize = 14
+    print (contents & 
+        init & 
+        slidingWindow windowSize & 
+        List.findIndex areElementsUnique & 
+        fromJust &
+        (+ windowSize))
+
+areElementsUnique :: Ord a => [a] -> Bool
+areElementsUnique list = length (Set.fromList list) == length list
